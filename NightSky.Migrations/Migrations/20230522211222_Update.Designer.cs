a310f620708fcac75e95c102b38d2972c590d463
Piotr Szczeszek <piotr.szczeszek00@gmail.com>
2023-05-22 23:15:37 +0200

    FIX: Wyrównanie bazy

--- NightSky.Migrations/Migrations/20230522211222_Update.Designer.cs
+++ NightSky.Migrations/Migrations/20230522211222_Update.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NightSky.API.DAL;
 
@@ -10,9 +11,11 @@ using NightSky.API.DAL;
 namespace NightSky.Migrations.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230522211222_Update")]
+    partial class Update
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.5");
